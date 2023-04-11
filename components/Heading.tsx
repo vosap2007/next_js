c5112cdf0ab03c7bdbf8c604b567fdb700775c10
vosap2007 <65125720+vosap2007@users.noreply.github.com>
2023-04-11 09:32:33 +0300

    add import { FC } from 'react';

--- components/Heading.tsx
+++ components/Heading.tsx
@@ -1,4 +1,11 @@
-const Heading = ({ text, tag }) => {
+import { FC } from 'react';
+
+type headingType = {
+  text: string;
+  tag?: string;
+};
+
+const Heading: FC<headingType> = ({ text, tag }) => {
   const Tag = tag || 'h1';
   return (
     <>
